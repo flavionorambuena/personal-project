a8c0701e306ef47c39d23f39b07ade8f0cdf3508
Flavio Norambuena <55749551+flavionorambuena@users.noreply.github.com>
2025-06-19 21:05:25 -0400

    Add skeleton Angular 20 shop application

---
+++ shop-app/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list.component';
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent }
+];
+
+@NgModule({
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
